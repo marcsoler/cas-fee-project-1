f0e5d2060515bb54578804579e49545c416c4b26
Marc Solèr <marcsoler87@gmail.com>
2021-05-24 15:01:03 +0200

    Enables mode switching

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,1 +1,13 @@
-console.log('Hello World');
+const modeSelector = document.querySelector('#mode');
+
+function toggleMode() {
+    const defaultMode = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+    const selectedMode = modeSelector.options[modeSelector.selectedIndex].value;
+    document.body.classList.remove('dark', 'light');
+    document.body.classList.add(selectedMode === 'default' ? defaultMode : selectedMode);
+    // TODO: store users preference
+}
+
+toggleMode();
+
+modeSelector.addEventListener('input', toggleMode);
