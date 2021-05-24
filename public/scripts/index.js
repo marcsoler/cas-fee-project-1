b80053dec5f8ba403c9714f991a01e0741efff1f
Marc Solèr <marcsoler87@gmail.com>
2021-05-24 15:13:13 +0200

    Stores users mode selection

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,11 +1,17 @@
 const modeSelector = document.querySelector('#mode');
 
 function toggleMode() {
-    const defaultMode = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+    let defaultMode;
+    if (localStorage.getItem('mode')) {
+        defaultMode = localStorage.getItem('mode');
+    } else {
+        defaultMode = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+    }
     const selectedMode = modeSelector.options[modeSelector.selectedIndex].value;
+    const mode = selectedMode === 'default' ? defaultMode : selectedMode;
     document.body.classList.remove('dark', 'light');
-    document.body.classList.add(selectedMode === 'default' ? defaultMode : selectedMode);
-    // TODO: store users preference
+    document.body.classList.add(mode);
+    localStorage.setItem('mode', mode);
 }
 
 toggleMode();
